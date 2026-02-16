eab58386dfa598dc80ae04610310b7f23f291ba6
sisterfuture <etakka@gmail.com>
2026-02-16 15:39:03 +0800

    fix: implement missing shouldInclude() and fix system prompt manager access in GetCurrentSystemPromptTool

--- app/src/main/java/com/stupidbeauty/sisterfuture/tool/GetCurrentSystemPromptTool.java
+++ app/src/main/java/com/stupidbeauty/sisterfuture/tool/GetCurrentSystemPromptTool.java
@@ -1,6 +1,7 @@
 package com.stupidbeauty.sisterfuture.tool;
 
 import com.stupidbeauty.sisterfuture.SisterFutureApplication;
+import com.stupidbeauty.sisterfuture.SystemPromptManager;
 import com.stupidbeauty.sisterfuture.tool.Tool;
 
 import org.json.JSONObject;
@@ -18,9 +19,15 @@ public class GetCurrentSystemPromptTool implements Tool {
     }
 
     @Override
+    public boolean shouldInclude() {
+        return true;
+    }
+
+    @Override
     public JSONObject execute(JSONObject arguments) {
         try {
-            return new JSONObject().put("current_prompt", application.getSystemPromptManager().getCurrentPrompt());
+            SystemPromptManager promptManager = SystemPromptManager.getInstance(application);
+            return new JSONObject().put("current_prompt", promptManager.getCurrentPrompt());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
