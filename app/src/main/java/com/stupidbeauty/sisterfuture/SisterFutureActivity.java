5f93801af2266f10e6a5c78786739d36c3ce3112
sisterfuture <etakka@gmail.com>
2026-02-27 17:15:53 +0800

    fix(activity): add missing imports for new tools

--- app/src/main/java/com/stupidbeauty/sisterfuture/SisterFutureActivity.java
+++ app/src/main/java/com/stupidbeauty/sisterfuture/SisterFutureActivity.java
@@ -605,7 +605,6 @@ public class SisterFutureActivity extends Activity implements TextToSpeech.OnIni
           Log.d(TAG, CodePosition.newInstance().toString() + ", adding message with role: " + messageRole + ", content: " + messageContent + ", tool call id: " + toolCAllId); // Debug.
 
 
-
           messagesArray.put(historyArray.getJSONObject(i));
         }
       }
