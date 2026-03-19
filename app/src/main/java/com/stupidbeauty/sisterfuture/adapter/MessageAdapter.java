52389786eb8632c3a00524ab4fb91c0e470b833f
sisterfuture <etakka@gmail.com>
2026-03-19 21:45:56 +0800

    Fix #4794: Restore MessageAdapter and implement line limiting
    - Restore complete MessageAdapter.java from master branch
    - Fix Lambda syntax: use v -> instead of v => (Java syntax)
    - Implement line limiting for ToolCallResultViewHolder:
      - Default: show max 10 lines
      - Click to toggle expand/collapse
      - Show "... (click to expand)" when truncated
    - Keep full message history intact (UI layer only)

--- app/src/main/java/com/stupidbeauty/sisterfuture/adapter/MessageAdapter.java
+++ app/src/main/java/com/stupidbeauty/sisterfuture/adapter/MessageAdapter.java
@@ -1,18 +1,217 @@
-public class ToolCallResultViewHolder extends RecyclerView.ViewHolder {
+package com.stupidbeauty.sisterfuture.adapter;
+
+import com.stupidbeauty.sisterfuture.bean.MessageItem;
+import com.stupidbeauty.sisterfuture.bean.MessageType;
+import androidx.recyclerview.widget.RecyclerView;
+import android.text.Spannable;
+import android.text.Selection;
+import butterknife.ButterKnife;
+import com.stupidbeauty.sisterfuture.network.TongYiClient.OnResponseListener;
+import com.koushikdutta.async.http.server.AsyncHttpServer;
+import com.stupidbeauty.sisterfuture.R;
+import java.util.List;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.stupidbeauty.sisterfuture.R;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.noties.markwon.Markwon;
+import io.noties.markwon.core.CorePlugin;
+import io.noties.markwon.ext.strikethrough.StrikethroughPlugin;
+import io.noties.markwon.ext.tables.TablePlugin;
+
+public class MessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final int TYPE_USER = 0;
+    private static final int TYPE_AI = 1;
+    private static final int TYPE_TOOL_CALL_RESULT = 2;
+
+    private List<MessageItem> messages = new ArrayList<>();
+
+    public MessageItem getItem(int position) {
+        return messages.get(position);
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        if (viewType == TYPE_USER) {
+            View itemView = inflater.inflate(R.layout.item_user_message, parent, false);
+            return new UserMessageViewHolder(itemView);
+        } else if (viewType == TYPE_AI) {
+            View itemView = inflater.inflate(R.layout.item_ai_message, parent, false);
+            return new AIMessageViewHolder(itemView);
+        } else {
+            View itemView = inflater.inflate(R.layout.item_tool_call_result_message, parent, false);
+            return new ToolCallResultViewHolder(itemView);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        MessageItem message = messages.get(position);
+        if (holder instanceof UserMessageViewHolder) {
+            ((UserMessageViewHolder) holder).bind(message);
+        } else if (holder instanceof AIMessageViewHolder) {
+            ((AIMessageViewHolder) holder).bind(message);
+        } else if (holder instanceof ToolCallResultViewHolder) {
+            ((ToolCallResultViewHolder) holder).bind(message);
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        MessageType type = messages.get(position).getType();
+        switch (type) {
+            case USER: return TYPE_USER;
+            case AI: return TYPE_AI;
+            case TOOL_CALL_RESULT: return TYPE_TOOL_CALL_RESULT;
+            default: throw new IllegalArgumentException("Unknown message type");
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return messages.size();
+    }
+
+    public void addMessage(MessageItem message) {
+        messages.add(message);
+        notifyItemInserted(messages.size() - 1);
+    }
+
+    public void updateAiMessage(int position, String newText) {
+        MessageItem item = messages.get(position);
+        if (item.getType() == MessageType.AI) {
+            item.text = newText;
+            notifyItemChanged(position);
+        }
+    }
+
+    public static class UserMessageViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.user_text) TextView textView;
+
+        public UserMessageViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            textView.setCustomSelectionActionModeCallback(new ActionMode.Callback() {
+                @Override
+                public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                    return true;
+                }
+
+                @Override
+                public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                    return false;
+                }
+
+                @Override
+                public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                    if (item.getItemId() == android.R.id.copy) {
+                        String selectedText = textView.getText().toString();
+                        ClipboardManager clipboard = (ClipboardManager)itemView.getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+                        android.content.ClipData clip = android.content.ClipData.newPlainText("selected text", selectedText);
+                        clipboard.setPrimaryClip(clip);
+                        mode.finish();
+                        return true;
+                    }
+                    return false;
+                }
+
+                @Override
+                public void onDestroyActionMode(ActionMode mode) {
+                    Spannable spannable = (Spannable)textView.getText();
+                    Selection.setSelection(spannable, 0, 0);
+                }
+            });
+        }
+
+        public void bind(MessageItem message) {
+            textView.setText(message.getText());
+        }
+    }
+
+    public static class AIMessageViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.ai_text) TextView textView;
+
+        private final Markwon markwon;
+
+        public AIMessageViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            markwon = Markwon.builder(itemView.getContext())
+                .usePlugin(CorePlugin.create())
+                .usePlugin(StrikethroughPlugin.create())
+                .usePlugin(TablePlugin.create(itemView.getContext()))
+                .build();
+            textView.setCustomSelectionActionModeCallback(new ActionMode.Callback() {
+                @Override
+                public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                    return true;
+                }
+
+                @Override
+                public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                    return false;
+                }
+
+                @Override
+                public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                    if (item.getItemId() == android.R.id.copy) {
+                        String selectedText = textView.getText().toString();
+                        ClipboardManager clipboard = (ClipboardManager)itemView.getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+                        android.content.ClipData clip = android.content.ClipData.newPlainText("selected text", selectedText);
+                        clipboard.setPrimaryClip(clip);
+                        mode.finish();
+                        return true;
+                    }
+                    return false;
+                }
+
+                @Override
+                public void onDestroyActionMode(ActionMode mode) {
+                    Spannable spannable = (Spannable)textView.getText();
+                    Selection.setSelection(spannable, 0, 0);
+                }
+            });
+        }
+
+        public void bind(MessageItem message) {
+            markwon.setMarkdown(textView, message.getText());
+        }
+    }
+
+    public static class ToolCallResultViewHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.tool_call_result_text) TextView textView;
         private static final int MAX_LINES = 10; // #4794: Maximum lines to display
+        private boolean isExpanded = false; // #4794: Track expand state
 
         public ToolCallResultViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
             
             // #4794: Add click listener to toggle expand/collapse
-            itemView.setOnClickListener(v => {
-                MessageItem item = (MessageItem) textView.getTag();
-                if (item != null) {
-                    item.isExpanded = !item.isExpanded;
-                    bind(item); // Re-bind with new state
-                }
+            itemView.setOnClickListener(v -> {
+                isExpanded = !isExpanded;
+                bind(getAdapterPosition());
             });
             
             textView.setCustomSelectionActionModeCallback(new ActionMode.Callback() {
@@ -48,16 +247,22 @@ public class ToolCallResultViewHolder extends RecyclerView.ViewHolder {
         }
 
         public void bind(MessageItem message) {
-            textView.setText(message.getText());
-            textView.setTag(message); // #4794: Save reference for click
-            
-            // #4794: Apply line limit based on expanded state
-            if (message.isExpanded) {
-                textView.setMaxLines(Integer.MAX_VALUE);
-                textView.setEllipsize(null);
+            String text = message.getText();
+            if (!isExpanded) {
+                // #4794: Limit to MAX_LINES
+                String[] lines = text.split("\n");
+                if (lines.length > MAX_LINES) {
+                    StringBuilder truncated = new StringBuilder();
+                    for (int i = 0; i < MAX_LINES; i++) {
+                        truncated.append(lines[i]).append("\n");
+                    }
+                    truncated.append("... (click to expand)");
+                    textView.setText(truncated.toString());
+                } else {
+                    textView.setText(text);
+                }
             } else {
-                textView.setMaxLines(MAX_LINES);
-                textView.setEllipsize(TextUtils.TruncateAt.END);
+                textView.setText(text);
             }
         }
     }
