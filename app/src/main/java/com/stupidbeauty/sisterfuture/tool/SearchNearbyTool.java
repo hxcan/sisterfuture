21fb027b2bbbd7ea27ab3fd6c8885356a70055ab
sisterfuture <etakka@gmail.com>
2026-04-27 09:27:21 +0800

    fix: 修正 OnGetPoiSearchResultListener 接口实现
    问题：之前使用的是 PoiDetailResult，但百度地图 SDK 7.5.4 中 onGetPoiDetailResult 方法的参数类型是 PoiDetailSearchResult。
    
    修复：将 import 和实现中的 PoiDetailResult 改为 PoiDetailSearchResult。

--- app/src/main/java/com/stupidbeauty/sisterfuture/tool/SearchNearbyTool.java
+++ app/src/main/java/com/stupidbeauty/sisterfuture/tool/SearchNearbyTool.java
@@ -11,7 +11,7 @@ import com.baidu.mapapi.search.poi.PoiResult;
 import com.baidu.mapapi.search.core.PoiInfo;
 import com.baidu.mapapi.search.core.SearchResult;
 import com.baidu.mapapi.search.poi.OnGetPoiSearchResultListener;
-import com.baidu.mapapi.search.poi.PoiDetailResult;
+import com.baidu.mapapi.search.poi.PoiDetailSearchResult;
 import com.baidu.mapapi.search.poi.PoiIndoorResult;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -56,12 +56,14 @@ public class SearchNearbyTool implements Tool {
             }
 
             @Override
-            public void onGetPoiDetailResult(PoiDetailResult result) {
+            public void onGetPoiDetailResult(PoiDetailSearchResult result) {
+                // POI 详情检索结果回调，此处不需要处理
                 Log.d(TAG, "onGetPoiDetailResult called");
             }
 
             @Override
             public void onGetPoiIndoorResult(PoiIndoorResult result) {
+                // 室内 POI 检索结果回调，此处不需要处理
                 Log.d(TAG, "onGetPoiIndoorResult called");
             }
         });
