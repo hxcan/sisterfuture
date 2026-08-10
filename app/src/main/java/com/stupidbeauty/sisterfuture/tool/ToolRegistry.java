eae2ff1b26bdd41005b09dcaa043c67e947451ca
sisterfuture <etakka@gmail.com>
2026-08-10 14:13:48 +0800

    refactor: 更新 ToolRegistry 的 import 路径，从 tools/ 改为 tool/

--- app/src/main/java/com/stupidbeauty/sisterfuture/tool/ToolRegistry.java
+++ app/src/main/java/com/stupidbeauty/sisterfuture/tool/ToolRegistry.java
@@ -4,8 +4,8 @@ import com.stupidbeauty.sisterfuture.ContextManager;
 import com.stupidbeauty.sisterfuture.manager.ModelAccessPointManager;
 import com.stupidbeauty.sisterfuture.manager.MemoryManager;
 import com.stupidbeauty.sisterfuture.SisterFutureApplication;
-import com.stupidbeauty.sisterfuture.tools.ListPhoneDirectoryTool;
-import com.stupidbeauty.sisterfuture.tools.ReadPhoneFileTool;
+import com.stupidbeauty.sisterfuture.tool.ListPhoneDirectoryTool;
+import com.stupidbeauty.sisterfuture.tool.ReadPhoneFileTool;
 import android.content.Context;
 
 
